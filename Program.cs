3a6caff3d5670799f62d891d3ebbd33c3aa93f82
OlgaAbramova86 <146665076+OlgaAbramova86@users.noreply.github.com>
2023-11-22 18:23:53 +0300

    Final

--- Program.cs
+++ Program.cs
@@ -3,12 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 
 
+// Задание 64
 
-
-//////////////////////////////////////////////
-void task64(int z)
-{
-    void Nto1(int N)
+    void fromNto1(int N)
     {
         Console.Write($"{N }");
         if (N > 1) Nto1(N-1);
@@ -19,13 +16,16 @@ void task64(int z)
     Console.WriteLine("Input N");
     int N = Convert.ToInt32(Console.ReadLine());
     Nto1(N);
-}
 
-/////////////////////////////////////////////////
 
-void task66(int z)
-{
-    int MtoNSum(int N, int M)
+
+
+
+
+// Задание 66
+
+
+    int SumMtoN(int N, int M)
     {
 
         if (N > M) return MtoNSum(N-1, M) + N;
@@ -40,13 +40,16 @@ void task66(int z)
         int N = Convert.ToInt32(Console.ReadLine());
         Console.Write(MtoNSum(N, M));
 
-}
 
-///////////////////////////////////////////////////
 
-void task68(int z)
-{
-    int Akkerman(int M, int N)
+
+
+
+
+// Задание 68
+
+
+    int Akk(int M, int N)
     {
         while(M != 0)
         {
@@ -55,25 +58,3 @@ void task68(int z)
             M -= 1;
         }
         return N+1;
-    }
-
-
-        Console.WriteLine("Input M");
-        int M = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Input N");
-        int N = Convert.ToInt32(Console.ReadLine());
-        Console.Write(Akkerman(M, N));
-
-}
-
-System.Console.WriteLine("какое задание запустить? (1, 2 или 3)");
-int z = Convert.ToInt32(Console.ReadLine());
-if (z == 1) task64(z);
-if (z == 2) task66(z);
-if (z == 3) task68(z);
-
-
-
-
-
-
